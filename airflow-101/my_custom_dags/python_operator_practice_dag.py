98e059eed0591228b89b5e1f06e1cac31810b73c
bilal ahmed <bilal.ahhmedd@gmail.com>
2022-10-16 15:37:31 +0500

    added my_custom_dags folder

---
+++ airflow-101/my_custom_dags/python_operator_practice_dag.py
@@ -0,0 +1,17 @@
+import datetime
+import pendulum
+
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+# [End import module]
+
+with DAG(
+
+
+
+) as dag:
+    
---
+++ airflow-101/py_scripts/task_1.py
@@ -0,0 +1,30 @@
+# [Start import module]
+import json
+import pendulum
+from airflow.decorators import dag, task
+
+# [End import module]
+
+@dag(
+    #schedule_interval=None,
+    start_date=pendulum.datetime(2022,10,14,tz='UTC'),
+    catchup=False,
+    tags=['example'],
+)
+def taskflow_api():
+    @task
+    def gen_data():
+        return [1,2,3,4]
+    @task
+    def process_data(data):
+        return [x**2 for x in data]
+    @task
+    def send_data(data):
+        print(data)
+    
+    
+    data=gen_data()
+    processed_data=process_data(data)
+    send_data(process_data)
+
+taskflow_api()
