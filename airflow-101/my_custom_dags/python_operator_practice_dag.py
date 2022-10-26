e9bc7893c615e8e3670a1c1aefd4a1c716d621e2
bilal ahmed <bilal.ahhmedd@gmail.com>
2022-10-26 21:39:39 +0500

    updated python operator dag, hello_world.py

--- airflow-101/my_custom_dags/python_operator_practice_dag.py
+++ airflow-101/my_custom_dags/python_operator_practice_dag.py
@@ -6,5 +6,33 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+#from ../py_scripts import hello_world
+
+class hello_world():
+
+    def print_hello_world():
+        print('hello world')
+
+    def print_my_world():
+        print('my world')
 
 # [End import module]
+dag_1 = DAG('hello_world', start_date=datetime.datetime(2022,10,25))
+
+'''
+with DAG(
+    'hello_world_dag',
+    start_date=datetime.date(2022,25,10)
+) as dag_1:
+    t1 = PythonOperator(task_id='hello_world_task',python_callable=hello_world.print_hello_world)
+    t2 = PythonOperator(task_id='my_world_task',python_callable=hello_world.print_my_world)
+
+    t1 >> t2
+'''
+
+
+hello_world_task = PythonOperator(task_id='hellow_world_task',python_callable=hello_world.print_hello_world,dag=dag_1)
+my_world_task = PythonOperator(task_id='my_world_task',python_callable=hello_world.print_my_world,dag=dag_1)
+
+hello_world_task >> my_world_task
---
+++ airflow-101/py_scripts/hello_world.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def print_hello_world():
+    print('hello world')
+
+def print_my_world():
+    print('my world')
