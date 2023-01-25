9d60df81252332c7be8e2ee8d4969c21cb8bf58d
bilal ahmed <bilal.ahhmedd@gmail.com>
2023-01-25 22:51:04 +0500

    modified:   airflow-101/my_custom_dags/python_operator_practice_dag.py 	new file:   pyspark-101/totorial-1/Code/__MACOSX/._pysparkapi 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._Dockerfile 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._README.md 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._backfill.py 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._crontab.txt 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._daily_spark_batch.py 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._main.py 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._requirements.txt 	new file:   pyspark-101/totorial-1/Code/__MACOSX/pysparkapi/._source_processing.py 	new file:   pyspark-101/totorial-1/Code/pysparkapi/Dockerfile 	new file:   pyspark-101/totorial-1/Code/pysparkapi/README.md 	new file:   pyspark-101/totorial-1/Code/pysparkapi/backfill.py 	new file:   pyspark-101/totorial-1/Code/pysparkapi/crontab.txt 	new file:   pyspark-101/totorial-1/Code/pysparkapi/daily_spark_batch.py 	new file:   pyspark-101/totorial-1/Code/pysparkapi/main.py 	new file:   pyspark-101/totorial-1/Code/pysparkapi/requirements.txt 	new file:   pyspark-101/totorial-1/Code/pysparkapi/source_processing.py 	new file:   pyspark-101/totorial-1/Data/Data/banktxn.csv 	new file:   pyspark-101/totorial-1/Data/__MACOSX/Data/._banktxn.csv 	new file:   pyspark-101/totorial-1/Solution Methodology.pdf 	new file:   pyspark-101/totorial-1/notes.txt

--- airflow-101/my_custom_dags/python_operator_practice_dag.py
+++ airflow-101/my_custom_dags/python_operator_practice_dag.py
@@ -18,21 +18,24 @@ class hello_world():
         print('my world')
 
 # [End import module]
-dag_1 = DAG('hello_world', start_date=datetime.datetime(2022,10,25))
+#dag_1 = DAG('hello_world', start_date=datetime.datetime(2022,10,25))
 
-'''
+
+
+
 with DAG(
     'hello_world_dag',
-    start_date=datetime.date(2022,25,10)
+    start_date=datetime.datetime(2022,10,25)
 ) as dag_1:
     t1 = PythonOperator(task_id='hello_world_task',python_callable=hello_world.print_hello_world)
     t2 = PythonOperator(task_id='my_world_task',python_callable=hello_world.print_my_world)
 
     t1 >> t2
+
+
 '''
-
-
 hello_world_task = PythonOperator(task_id='hellow_world_task',python_callable=hello_world.print_hello_world,dag=dag_1)
 my_world_task = PythonOperator(task_id='my_world_task',python_callable=hello_world.print_my_world,dag=dag_1)
 
 hello_world_task >> my_world_task
+'''
